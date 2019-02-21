fe474aaf30d501d662994de6f314022b26d4a919
zdyGit <1193009984@qq.com>
2019-02-21 15:02:43 +0800

    init aipc proj

---
+++ src/main/java/com/zdy/aipc/AipcWithJavaApplication.java
@@ -0,0 +1,13 @@
+package com.zdy.aipc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AipcWithJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AipcWithJavaApplication.class, args);
+	}
+
+}
